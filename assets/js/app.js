3c6984a97cef89f9ed58dee1c55e8d95d61c2576
Alvaro Masitsa <alvaromasitsa104@gmail.com>
2015-04-20 10:07:56 +0300

    Singles app

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,97 +22,47 @@ $(document).on("submit","form#signin-client",function(e)
 	e.preventDefault();
 	
 	//check if there is a network connection
-	var connection = is_connected();
+	var connection = true;//is_connected();
 	
-	if(connection == true)
+	if(connection === true)
 	{
-		var form_data = new FormData(this);
+		//var form_data = new FormData(this);
 		var form_action = $(this).attr('action');
-	
+		//$("#response").fadeOut( "slow").html('');
+		
+		//get form values
+		var email = $("input[name=client_email]").val();
+		var password = $("input[name=client_password]").val();
+		
 		$.ajax({
 			type:'POST',
-			url: base_url+form_action,
-			data:form_data,
+			url: base_url+form_action+"/"+email+"/"+password+"?callback=?",
 			cache:false,
 			contentType: false,
 			processData: false,
 			dataType: 'json',
 			success:function(data)
-			{	
+			{
 				if(data.message == "success")
 				{
-					$("#main_body").html(data.result.client_username);
+					//$("#response").html(data.result.client_username).fadeIn( "slow");
+					window.location.href = 'pages/profiles.html';
 				}
 				else
 				{
-					$("#main_body").html(data.result);
+					$("#response").html('<div class="alert alert-danger center-align">'+data.result+'</div>').fadeIn( "slow");
 				}
 			},
 			error: function(xhr, status, error) 
 			{
-				$("#main_body").html(error);
+				$("#response").html('<div class="alert alert-danger center-align">'+error+'</div>').fadeIn( "slow");
 			}
 		});
 	}
 	
 	else
 	{
-		alert("No internet connection - please check your internet connection then try again");
+		$("#response").html('<div class="alert alert-danger center-align">'+"No internet connection - please check your internet connection then try again"+'</div>').fadeIn( "slow");
 	}
 	return false;
-});
-
-run(function () {
-    // immediately invoked on first run
-    var init = (function () {
-        navigator.network.isReachable(base_url, function(status) {
-			var connectivity = (status.internetConnectionStatus || status.code || status);
-        	if (connectivity === NetworkStatus.NOT_REACHABLE) {
-        		alert("No internet connection - we won't be able to show you any maps");
-        	} else {
-        		alert("We can reach Google - get ready for some awesome maps!");
-        	}
-        });
-    })();
-    
-    // a little inline controller
-    when('#welcome');
-    when('#settings', function() {
-		// load settings from store and make sure we persist radio buttons.
-		store.get('config', function(saved) {
-			if (saved) {
-				if (saved.map) {
-					x$('input[value=' + saved.map + ']').attr('checked',true);
-				}
-				if (saved.zoom) {
-					x$('input[name=zoom][value="' + saved.zoom + '"]').attr('checked',true);
-				}
-			}
-		});
-	});
-    when('#map', function () {
-        store.get('config', function (saved) {
-            // construct a gmap str
-            var map  = saved ? saved.map || ui('map') : ui('map')
-            ,   zoom = saved ? saved.zoom || ui('zoom') : ui('zoom')
-            ,   path = "http://maps.google.com/maps/api/staticmap?center=";
-			
-            navigator.geolocation.getCurrentPosition(function (position) {
-                var location = "" + position.coords.latitude + "," + position.coords.longitude;
-                path += location + "&zoom=" + zoom;
-                path += "&size=250x250&maptype=" + map + "&markers=color:red|label:P|";
-                path += location + "&sensor=false";
-
-                x$('img#static_map').attr('src', path);
-            });
-        });
-    });
-    when('#save', function () {
-        store.save({
-            key:'config',
-            map:ui('map'),
-            zoom:ui('zoom')
-        });
-        display('#welcome');
-    });
 });
